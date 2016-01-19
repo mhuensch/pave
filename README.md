59b13e37a8c13c9170ea670053a03561ad2215f6
Michael Huensch <setup@run00.com>
2016-01-18 20:23:23 -0600

    Updating config

--- README.md
+++ README.md
@@ -5,15 +5,5 @@ Script to setup new install of OSX
 mkdir -p /tmp/pave \
 && curl -L -o /tmp/pave/pave.zip "https://github.com/run00/brew/archive/master.zip" \
 && unzip -q -o -d /tmp/pave /tmp/pave/pave.zip \
-&& sh /tmp/pave/brew-master/install.sh \
-&& sh /tmp/pave/brew-master/config.sh
+&& sh /tmp/pave/brew-master/install.sh
 ```
-
-# To run this file, you must disable System Integrity Protection
-# 1. Restart
-# 2. As soon as black screen appears, hold down [command] + [R]
-# 3. Click Utilities > Terminal
-# 4. Run csrutil disable
-# 5. Run this file
-# 6. Follow steps 1-3 above
-# 7. Run csrutil enable
--- install.sh
+++ install.sh
@@ -8,10 +8,18 @@ echo "  RUNNING OSX INSTALL & CONFIGURATION"
 echo "---------------------------------------"
 echo ""
 
+
 csrutil_status=$(csrutil status | awk '{print $5}')
 if [[ $csrutil_status = 'enabled.' ]]; then
 	echo "System Integrity Protection must be disabled before running this script."
-	echo "(see README.md for more information)"
+	echo " Follow the following steps:"
+	echo " 1. Restart"
+	echo " 2. As soon as black screen appears, hold down [command] + [R]"
+	echo " 3. Click Utilities > Terminal"
+	echo " 4. Run csrutil disable"
+	echo " 5. Run this file"
+	echo " 6. Follow steps 1-3 above"
+	echo " 7. Run csrutil enable"
 	return
 fi
 
@@ -29,14 +37,17 @@ rm Alfred.zip
 mv "Alfred 2.app" /Applications
 rm -fr 'Alfred 2.app'
 
+
 installing "XCode"
 xcode-select --install
 read -p "Press [Enter] after xcode is installed"
+
 
 installing "Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Update homebrew recipes
 brew update
+
 
 installing "Homebrew Cask"
 brew install caskroom/cask/brew-cask
@@ -51,6 +62,7 @@ brew tap caskroom/fonts
 
 
 
+
 # APPLICATIONS
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
@@ -58,9 +70,11 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 installing "Atom"
 brew cask install atom
 
+
 installing "Bartender"
 brew cask install bartender
 # SEE: https://www.macbartender.com/B2/system-item-setup/ to disable spotlight
+
 
 installing "CCleaner"
 brew cask install ccleaner
@@ -113,7 +127,7 @@ brew cask install microsoft-lync
 installing "Mongo Managment Studio"
 brew cask install mongo-management-studio
 
-l
+
 installing "Moom"
 brew cask install moom
 
@@ -168,3 +182,152 @@ sudo rm -rf "/Applications/Time Machine.app"
 
 brew cask cleanup
 brew cleanup
+
+
+
+
+# CONFIGURATION
+
+echo ""
+echo "Hide unwanted icons"
+for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+  defaults write "${domain}" dontAutoLoad -array \
+    "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+    "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+    "/System/Library/CoreServices/Menu Extras/User.menu"
+done
+
+# defaults write com.apple.systemuiserver menuExtras -array \
+#   "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
+
+echo ""
+echo "Disable spotlight"
+sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+
+
+echo ""
+echo "Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo ""
+echo "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+echo ""
+echo "Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo ""
+echo "Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window"
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+
+
+echo ""
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+echo ""
+echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+
+echo ""
+echo "Use list view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Nlsv
+
+echo ""
+echo "Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo ""
+echo "Showing status bar in Finder by default"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo ""
+echo "Allowing text selection in Quick Look/Preview in Finder by default"
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+echo ""
+echo "Displaying full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo ""
+echo "Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo ""
+echo "Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+
+
+echo ""
+echo "Wipe all (default) app icons from the Dock"
+defaults write com.apple.dock persistent-apps -array
+
+echo ""
+echo "Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+echo ""
+echo "Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+echo ""
+echo "Preventing Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+echo ""
+echo "Disabling local Time Machine backups"
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+echo ""
+echo "Disable annoying backswipe in Chrome"
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+
+echo ""
+echo "Disable boot chime"
+sudo nvram SystemAudioVolume=%00
+
+echo ""
+echo "Switch to dark mode"
+sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
+
+echo ""
+echo "Disable guest account"
+sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+sudo defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool NO
+
+echo ""
+echo "Set mouse scroll direction to normal"
+sudo defaults write -g com.apple.swipescrolldirection -bool FALSE
+
+echo ""
+echo "Disable annoying sounds"
+user=`ls -l /dev/console | cut -d " " -f4`
+
+# Turn off "Play feedback when volume is changed
+sudo "${user}" -c 'defaults write -g com.apple.sound.beep.feedback -integer 0'
+
+# Turn off "Play user interface sound effects
+sudo "${user}" -c 'defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0'
+
+# Turn the volume down the alert volume
+sudo "${user}" -c 'defaults write com.apple.systemsound com.apple.sound.beep.volume -float 0'
+
+
+cp -a -f -n .bash_profile ~/
+
+
+secs=5
+while [ $secs -gt 0 ]; do
+   echo -ne "Restarting in: $secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
+sudo shutdown -r now
